6b6f9785da7a4e34dd528196a52c2d7b400bda32
紫英 <ziying.wzc@taobao.com>
2019-03-12 17:57:04 +0800

    :memo: add more ts typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -27,18 +27,34 @@ declare class Consumer {
     isSubscribeTopicNeedUpdate(topic: string): boolean;
 }
 
+interface BaseOptions {
+    accessKey: string;
+    secretKey: string;
+    topic: string;
+    onsAddr?: string;
+    instanceName?: string;
+    pollNameServerInteval?: number;
+    heartbeatBrokerInterval?: number;
+    persistConsumerOffsetInterval?: number;
+    rebalanceInterval?: number;
+    clientIP?: string;
+    unitMode?: boolean;
+    onsChannel?: string;
+    [key: string]: any;
+}
+
 declare namespace Producer {
-    export interface ProducerOptions {
-        logger: any;
+    export interface ProducerOptions extends BaseOptions {
+        logger?: any;
         producerGroup: string;
-        createTopicKey: string;
-        defaultTopicQueueNums: number;
-        sendMsgTimeout: number;
-        compressMsgBodyOverHowmuch: number;
-        retryTimesWhenSendFailed: number;
-        retryAnotherBrokerWhenNotStoreOK: boolean;
-        maxMessageSize: number;
-        connectTimeout: number;
+        createTopicKey?: string;
+        defaultTopicQueueNums?: number;
+        sendMsgTimeout?: number;
+        compressMsgBodyOverHowmuch?: number;
+        retryTimesWhenSendFailed?: number;
+        retryAnotherBrokerWhenNotStoreOK?: boolean;
+        maxMessageSize?: number;
+        connectTimeout?: number;
     }
 
     export interface SendResult {
@@ -59,31 +75,31 @@ declare namespace Producer {
 }
 
 declare namespace Consumer {
-    export interface ComsumerOptions {
-        logger: any;
-        persistent: boolean // 是否持久化消费进度
-        isBroadcast: boolean; // 是否是广播模式（默认集群消费模式）
-        brokerSuspendMaxTimeMillis: number; // 长轮询模式，Consumer连接在Broker挂起最长时间
-        pullTimeDelayMillsWhenException: number; // 拉消息异常时，延迟一段时间再拉
-        pullTimeDelayMillsWhenFlowControl: number; // 进入流控逻辑，延迟一段时间再拉
-        consumerTimeoutMillisWhenSuspend: number; // 长轮询模式，Consumer超时时间（必须要大于brokerSuspendMaxTimeMillis）
+    export interface ComsumerOptions extends BaseOptions {
+        logger?: any;
+        persistent?: boolean // 是否持久化消费进度
+        isBroadcast?: boolean; // 是否是广播模式（默认集群消费模式）
+        brokerSuspendMaxTimeMillis?: number; // 长轮询模式，Consumer连接在Broker挂起最长时间
+        pullTimeDelayMillsWhenException?: number; // 拉消息异常时，延迟一段时间再拉
+        pullTimeDelayMillsWhenFlowControl?: number; // 进入流控逻辑，延迟一段时间再拉
+        consumerTimeoutMillisWhenSuspend?: number; // 长轮询模式，Consumer超时时间（必须要大于brokerSuspendMaxTimeMillis）
         consumerGroup: string;
-        consumeFromWhere: string; // Consumer第一次启动时，从哪里开始消费
+        consumeFromWhere?: string; // Consumer第一次启动时，从哪里开始消费
         /**
          * Consumer第一次启动时，如果回溯消费，默认回溯到哪个时间点，数据格式如下，时间精度秒：
          * 20131223171201
          * 表示2013年12月23日17点12分01秒
          * 默认回溯到相对启动时间的半小时前
          */
-        consumeTimestamp: number;
-        pullThresholdForQueue: number; // 本地队列消息数超过此阀值，开始流控
-        pullInterval: number; // 拉取消息的频率, 如果为了降低拉取速度，可以设置大于0的值
-        consumeMessageBatchMaxSize: number; // 消费一批消息，最大数
-        pullBatchSize: number; // 拉消息，一次拉多少条
-        parallelConsumeLimit: number; // 并发消费消息限制
-        postSubscriptionWhenPull: boolean; // 是否每次拉消息时，都上传订阅关系
-        allocateMessageQueueStrategy: any; // 队列分配算法，应用可重写
-        maxReconsumeTimes: number; // 最大重试次数
+        consumeTimestamp?: number;
+        pullThresholdForQueue?: number; // 本地队列消息数超过此阀值，开始流控
+        pullInterval?: number; // 拉取消息的频率, 如果为了降低拉取速度，可以设置大于0的值
+        consumeMessageBatchMaxSize?: number; // 消费一批消息，最大数
+        pullBatchSize?: number; // 拉消息，一次拉多少条
+        parallelConsumeLimit?: number; // 并发消费消息限制
+        postSubscriptionWhenPull?: boolean; // 是否每次拉消息时，都上传订阅关系
+        allocateMessageQueueStrategy?: any; // 队列分配算法，应用可重写
+        maxReconsumeTimes?: number; // 最大重试次数
     };
 }
 
